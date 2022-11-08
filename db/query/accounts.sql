8c8810c5998313bc2a6070ddc69790c19bdadb3b
Mosaab <musab.ehab@gmail.com>
2022-11-08 18:27:21 +0200

    add sqlc and queries

---
+++ db/query/accounts.sql
@@ -0,0 +1,20 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+    owner,
+    balance,
+    currency
+) VALUES (
+    $1,$2,$3
+) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * from accounts where id = $1 limit 1;
+
+-- name: ListAccounts :many
+select * from accounts order by id desc limit $1 offset $2;
+
+-- name: UpdateAccount :one
+update accounts set balance = $2 where id = $1 RETURNING *;
+
+-- name: DeleteAccount :exec
+delete from accounts where id = $1;
